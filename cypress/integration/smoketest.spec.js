7b58cd43316c045027a2904a1eb6a69dca7f5d76
Tim McLaren <timm@benon.com>
2019-11-27 15:35:16 +1000

    Initial commit

--- cypress/integration/smoketest.spec.js
+++ cypress/integration/smoketest.spec.js
@@ -14,3 +14,41 @@ describe("Smoke test.", () => {
 		cy.title().should("contain", "Oz Lotteries");
 	});
 });
+
+describe("test", () => {
+	const email = 'email1@email.com';
+	const password = 'password123';
+
+	it('Should register', () => {
+		cy.visit("/");
+		cy.get('#header_login_button').click();
+		cy.get('#loginFormContainer_registerLink').click();
+		cy.get('#signup_email').type(email);
+		cy.get('#signup_password').type(password);
+		cy.get('#signup_first_name').type('john');
+		cy.get('#signup_last_name').type('smith');
+		cy.get('#dob_day').select('01');
+		cy.get('#dob_month').select('01');
+		cy.get('#dob_year').select('2000');
+		cy.get('#address_autocomplete').type('123 Eagle Street');
+		cy.get('#signup_suburb').type('Brisbane City');
+		cy.get('#signup_state').type('QLD');
+		cy.get('#signup_postcode').type('4000');
+		cy.get('#country').select('AU');
+		cy.get('#signup_phone').type('0412345678');
+		cy.get('#signup_agree').check({force: true});
+		cy.get('[data-id=signup_submit_button]').click();
+
+		cy.get("#nav_logout_link").should("exist");
+	});
+
+	it('Should sign-in', () => {
+		cy.visit("/");
+		cy.get('#header_login_button').click();
+		cy.get('#dynamic_customer_login_form_email').type(email);
+		cy.get('#dynamic_customer_login_form_password').type(password);
+		cy.get('[data-id=dynamic_customer_login_form_submitButton]').click();
+		
+		cy.get("#nav_logout_link").should("exist");
+	});
+});
