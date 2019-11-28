3f677581f122b36448113ced261da7f098cd6cf0
bradf <bradf@benon.com>
2019-11-28 16:17:07 +1000

    Added the completed version of the workshop

--- cypress/integration/RegisterLogin.spec.js
+++ cypress/integration/RegisterLogin.spec.js
@@ -1,5 +1,21 @@
 /// <reference types="Cypress" />
 
+import faker from "faker";
+
+function createNewUserDetails() {
+	const email = `test_${Date.now()}@benon.com`;
+
+	return {
+		firstName: faker.name.firstName(),
+		lastName: faker.name.lastName(),
+		password: "4Me2Test",
+		streetAddress: "488 George Street",
+		phone: faker.phone.phoneNumber(),
+		email
+	};
+}
+const userDetails = createNewUserDetails();
+
 describe("Register and login test", () => {
 	it("should Register correctly", () => {
 		cy.visit("/");
@@ -7,27 +23,28 @@ describe("Register and login test", () => {
 		cy.get("#header_login_button").click();
 		cy.get("#loginFormContainer_registerLink").click();
 		cy.url().should("contain", "signup");
-		cy.get("#signup_email").type("bradf+7@benon.com");
-		cy.get("#signup_password").type("4Me2Test");
-		cy.get("#signup_first_name").type("Brad");
-		cy.get("#signup_last_name").type("Forrest");
-		cy.get("#dob_day").select("01");
-		cy.get("#dob_month").select("01");
-		cy.get("#dob_year").select("1988");
-		cy.get("#address_autocomplete").type("488 George st");
-		cy.get("[data-id='addressAutoComplete_suggestionText']")
-			.first()
-			.click();
-		cy.get("#signup_phone").type("0400 000 000");
-		cy.get("#signup_agree_checkbox_button").click();
-		cy.get("#signup_form").submit();
+		// cy.get("#signup_email").type(userDetails.email);
+		// cy.get("#signup_password").type(userDetails.password);
+		// cy.get("#signup_first_name").type(userDetails.firstName);
+		// cy.get("#signup_last_name").type(userDetails.lastName);
+		// cy.get("#dob_day").select("01");
+		// cy.get("#dob_month").select("01");
+		// cy.get("#dob_year").select("1988");
+		// cy.get("#address_autocomplete").type(userDetails.streetAddress);
+		// cy.get("[data-id='addressAutoComplete_suggestionText']")
+		// 	.first()
+		// 	.click();
+		// cy.get("#signup_phone").type(userDetails.phone);
+		// cy.get("#signup_agree_checkbox_button").click();
+		// cy.get("#signup_form").submit();
+		cy.fillRegistrationFormAndSubmit(userDetails);
 	});
 	it("should login correctly", () => {
 		cy.visit("/");
 		cy.get("#header_login_button").click();
 		cy.get("#dynamic_customer_login_form_form").within(() => {
-			cy.get("#dynamic_customer_login_form_email").type("bradf+7@benon.com");
-			cy.get("#dynamic_customer_login_form_password").type("4Me2Test");
+			cy.get("#dynamic_customer_login_form_email").type(userDetails.email);
+			cy.get("#dynamic_customer_login_form_password").type(userDetails.password);
 			cy.get("[data-id='dynamic_customer_login_form_submitButton']").click();
 		})
 		cy.get("#nav_logout_link");
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,23 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("fillRegistrationFormAndSubmit", userDetails => {
+	cy.get("#signup_email").type(userDetails.email);
+	cy.get("#signup_password").type(userDetails.password);
+	cy.get("#signup_first_name").type(userDetails.firstName);
+	cy.get("#signup_last_name").type(userDetails.lastName);
+	cy.get("#dob_day").select("01");
+	cy.get("#dob_month").select("01");
+	cy.get("#dob_year").select("1988");
+	cy.get("#address_autocomplete").type(userDetails.streetAddress);
+	cy.getByDataId("addressAutoComplete_suggestionText")
+		.first()
+		.click();
+	cy.get("#signup_phone").type(userDetails.phone);
+	cy.get("#signup_agree_checkbox_button").click();
+	// cy.wait(500);
+	cy.get("#signup_form").submit();
+});
+Cypress.Commands.add("getByDataId", dataId => {
+	cy.get(`[data-id='${dataId}']`);
+});
