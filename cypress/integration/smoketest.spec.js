29a0b8d41d6fdbba96735d1c7e9a200e514e2617
Michelle Strudwick <michelles@benon.com>
2019-11-28 13:41:10 +1000

    Initial commit

--- cypress/integration/smoketest.spec.js
+++ cypress/integration/smoketest.spec.js
@@ -14,3 +14,42 @@ describe("Smoke test.", () => {
 		cy.title().should("contain", "Oz Lotteries");
 	});
 });
+
+const time = Date.now();
+const email = `michelles${time}@benon.com`
+
+describe("Registration", () => {
+	it("should register new customer", () => {
+		cy.visit("/");
+		cy.get("#header_login_button").click();
+		cy.get("#loginFormContainer_registerLink").click();
+		cy.get("#signup_email").type(email);
+		cy.get("#signup_password").type('go4jumbo');
+		cy.get("#title").select('Ms');
+		cy.get("#signup_first_name").type('Michelle');
+		cy.get("#signup_last_name").type('Test');
+		cy.get("#dob_day").select('1');
+		cy.get("#dob_month").select('January');
+		cy.get("#dob_year").select('2001');
+		cy.get("#address_autocomplete").type('1');
+		cy.get("[data-id='addressAutoComplete_suggestion']").first().click();
+		cy.get("#signup_phone").type('0411200100');
+		cy.get("#signup_agree").check({ force: true });
+		cy.get("[data-id='signup_submit_button']").click();
+		cy.wait(2000);
+		cy.url().should('include', '/my-account');
+		cy.get("#nav_logout_link").click();
+	});
+});
+
+describe("Login after successful registration", () => {
+	it("should be able to login new customer", () => {
+		cy.visit("/");
+		cy.get("#header_login_button").click();
+		cy.get("#dynamic_customer_login_form_email").type(email);
+		cy.get("#dynamic_customer_login_form_password").type('go4jumbo');
+		cy.get("[data-id='dynamic_customer_login_form_submitButton']").click();
+		cy.wait(2000);
+		cy.get("#nav_logout_link").should('exist');
+	});
+})
